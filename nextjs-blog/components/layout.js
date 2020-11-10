2d484962acc9bb0444eb61ca667e6ffd72454d07
Nattaphong Klinjan <nattap1901@hotmail.com>
2020-11-10 18:06:35 +0100

    Added simple layout

--- nextjs-blog/components/layout.js
+++ nextjs-blog/components/layout.js
@@ -1,16 +1,9 @@
 import { useState } from 'react';
 import Head from 'next/head';
-import { Layout, Menu } from 'antd';
+import { Layout, Menu} from 'antd';
 export const siteTitle = 'THAKC - Thailands artisan keycaps community';
-import {
-  MenuUnfoldOutlined,
-  MenuFoldOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-  UploadOutlined,
-} from '@ant-design/icons';
 
-const { Header, Sider, Content } = Layout;
+const { Header, Content, Footer } = Layout;
 
 
 export default function MainLayout({ children, home }) {
@@ -38,51 +31,28 @@ export default function MainLayout({ children, home }) {
         <meta name='twitter:card' content='summary_large_image' />
       </Head>
 
-      <Layout>
-        <Sider trigger={null} collapsible collapsed={collapsed}>
+      <Layout style={{ height: '100vh'}}>
+        <Header>
           <div className="logo" />
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-            <Menu.Item key="1" icon={<UserOutlined />}>
-              nav 1
-            </Menu.Item>
-            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-              nav 2
-            </Menu.Item>
-            <Menu.Item key="3" icon={<UploadOutlined />}>
-              nav 3
-            </Menu.Item>
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+            <Menu.Item key="1">All</Menu.Item>
+            <Menu.Item key="2">Thai Artists</Menu.Item>
+            <Menu.Item key="3">International Artist</Menu.Item>
           </Menu>
-        </Sider>
-        <Layout className="site-layout">
-          <Header className="site-layout-background" style={{ 
-            padding: 0,
-            backgroundColor: '#FFF',
-            fontSize: 18,
-            lineHeight: '64px',
-            padding: '0 40px'
-            
-            }}>
-            {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-              className: 'trigger',
-              onClick: toggle,
-            })}
-          </Header>
-          <Content
-            className="site-layout-background"
-            style={{
-              margin: '24px 16px',
-              padding: 24,
-              minHeight: 280,
-            }}
-          >
-            {home}{children}
+        </Header>
+
+        <Content
+          className="site-layout-background"
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            minHeight: 280,
+          }}
+        >
+          {home}{children}
           </Content>
-        </Layout>
+        <Footer style={{ textAlign: 'center' }}>Created by Thailands Artisan Keycaps Community</Footer>
       </Layout>
-
-
     </>
-
- 
   );
 }
